e12aaf8b76f65fae25c37802efffbe269786e435
Nicolas Herbaut <nherbaut@labri.fr>
2016-07-19 14:14:19 +0200

    fixed sla

--- docker/build.sh
+++ docker/build.sh
@@ -4,6 +4,6 @@ mvn clean package
 cd docker/dockermgt
 docker build -t dngroup/simu-dockermgt .
 #docker push dngroup/simu-dockermgt
-cd ../
+cd ../session
 docker build -t dngroup/simu-sessionmgt .
 #docker push dngroup/simu-dockermgt 
